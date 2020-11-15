015b0e7182dd5b8a8071a3ed371cbfd74430e602
dsakda <74044765+dsakda@users.noreply.github.com>
2020-11-15 07:03:07 +0700

    Create Sale.java

---
+++ src/main/java/com/example/SpringBootCRUD/Sale.java
@@ -0,0 +1,51 @@
+package com.example.SpringBootCRUD;
+
+public class Sale {
+
+    private int Id;
+    private String item;
+    private int quantity;
+    private float amount;
+
+    public Sale() {
+    }
+
+    public Sale(int id, String item, int quantity, float amount) {
+        Id = id;
+        this.item = item;
+        this.quantity = quantity;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+}
