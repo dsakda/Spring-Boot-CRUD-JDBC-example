9fcb9d5810b1a51b658daa1f21ff268046de4a8b
dsakda <74044765+dsakda@users.noreply.github.com>
2020-11-17 21:18:10 +0700

    Update AppController.java

--- src/main/java/com/example/SpringBootCRUD/Controller/AppController.java
+++ src/main/java/com/example/SpringBootCRUD/Controller/AppController.java
@@ -1,5 +1,6 @@
-package com.example.SpringBootCRUD;
+package com.example.SpringBootCRUD.Controller;
 
+import com.example.SpringBootCRUD.SaleDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
