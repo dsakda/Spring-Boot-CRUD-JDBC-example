b6501f60e9408c86a357ec4c3470c91e49548698
dsakda <74044765+dsakda@users.noreply.github.com>
2020-11-19 08:52:06 +0700

    Update AppController.java

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -1,12 +1,24 @@
 package com.example.controller;
 
 import com.example.dao.SaleDAO;
+import com.example.model.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
 
     @Autowired
     private SaleDAO dao;
+
+    @RequestMapping("/")
+    public String viewHomePage(Model model) {
+        List<Sale> listSale = dao.list();
+        model.addAttribute("listSale", listSale);
+        return "index";
+    }
 }
