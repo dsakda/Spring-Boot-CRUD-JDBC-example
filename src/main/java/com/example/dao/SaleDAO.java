f0eee3e97519f4967b544793e5797da2e4aaacf4
dsakda <74044765+dsakda@users.noreply.github.com>
2020-11-19 08:52:12 +0700

    Update SaleDAO.java

--- src/main/java/com/example/dao/SaleDAO.java
+++ src/main/java/com/example/dao/SaleDAO.java
@@ -2,6 +2,7 @@ package com.example.dao;
 
 import com.example.model.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +17,12 @@ public class SaleDAO {
     private JdbcTemplate jdbcTemplate;
 
     public List<Sale> list() {
-        return null;
+
+        String sql = "SELECT * FROM SALES";
+
+        List<Sale> listSale = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Sale.class));
+
+        return listSale;
     }
 
     public void save(Sale sale) {
