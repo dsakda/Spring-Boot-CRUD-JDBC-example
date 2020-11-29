cf15ad5824a605b5c894c8f8779d2d27b163e95c
dsakda <74044765+dsakda@users.noreply.github.com>
2020-11-30 04:20:09 +0700

    Update AppController.java

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -5,7 +5,9 @@ import com.example.model.Sale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -18,7 +20,22 @@ public class AppController {
     @RequestMapping("/")
     public String viewHomePage(Model model) {
         List<Sale> listSale = dao.list();
+        System.out.println(listSale);
+
         model.addAttribute("listSale", listSale);
         return "index";
+    }
+
+    @RequestMapping("/new")
+    public String showNewForm(Model model) {
+        Sale sale = new Sale();
+        model.addAttribute("sale", sale);
+        return "new_form";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("sale") Sale sale) {
+        dao.save(sale);
+        return "redirect:/";
     }
 }
