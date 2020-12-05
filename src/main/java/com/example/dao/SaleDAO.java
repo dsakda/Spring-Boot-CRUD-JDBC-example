393bf8f75c470f33937bb7259b8fa674b236f140
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-05 07:28:23 +0700

    Update SaleDAO.java

--- src/main/java/com/example/dao/SaleDAO.java
+++ src/main/java/com/example/dao/SaleDAO.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +20,8 @@ public class SaleDAO {
     private JdbcTemplate jdbcTemplate;
 
     public List<Sale> list() {
-
         String sql = "SELECT * FROM SALES";
-
         List<Sale> listSale = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Sale.class));
-
         return listSale;
     }
 
@@ -31,19 +29,25 @@ public class SaleDAO {
         SimpleJdbcInsert insertActor = new SimpleJdbcInsert(jdbcTemplate);
         insertActor.withTableName("SALES").usingColumns("ITEM", "QUANTITY", "AMOUNT");
         BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(sale);
-
         insertActor.execute(parameterSource);
     }
 
     public Sale get(int id) {
-        return null;
+        String sql = "SELECT * FROM SALES WHERE id = ?";
+        Object[] args = {id};
+        Sale sale = jdbcTemplate.queryForObject(sql, args, BeanPropertyRowMapper.newInstance(Sale.class));
+        return sale;
     }
 
     public void update(Sale sale) {
-
+        String sql = "UPDATE SALES SET item=:item, quantity=:quantity, amount=:amount WHERE id=:id";
+        BeanPropertySqlParameterSource param = new BeanPropertySqlParameterSource(sale);
+        NamedParameterJdbcTemplate template = new NamedParameterJdbcTemplate(jdbcTemplate);
+        template.update(sql, param);
     }
 
     public void delete(int id) {
-
+        String sql = "DELETE FROM SALES WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
